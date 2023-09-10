225ff33ee1d880a6d81853dc68c094dcd4db297a
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-09-10 14:51:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const sentences = document.querySelectorAll('.line');
+
+    function showNextSentence() {
+        sentences[currentIndex].classList.remove('show');
+        currentIndex = (currentIndex + 1) % sentences.length;
+        sentences[currentIndex].classList.add('show');
+    }
+
+    let currentIndex = 0;
+    sentences[currentIndex].classList.add('show');
+    setInterval(showNextSentence, 3000);
+});
